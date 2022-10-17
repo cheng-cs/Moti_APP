ea3c92de811e4f7f9c3b7b1ff3f78938f655a208
cheng2 <cheng2@allegheny.edu>
2022-10-17 17:09:37 -0400

    version 1 complete.

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 import random
 import pandas as pd
-# import streamlit as st
+import streamlit as st
+import time
 
 
 def openF():
-    
-    df = pd.read_csv('quotes.csv' , delimiter=',')
 
-    # num = randomGenNum()
+    # create reset button here
 
-    # print(df.Author[num])
-    # print(df.Quote[num])
 
-    # print("Your quote: \n", df.Quote[num], "by: \n", df.Author[num])
 
-    # print using st.write. 
+    df = pd.read_csv('quotes2.csv' , delimiter=';')
 
     x = 1
-    y = 8
+    y = 6
     for x in range(1, y):
         num = randomGenNum()
-        print("Your quote: \n", df.Quote[num],"\n", "by: \n", df.Author[num])
+        st.markdown(df.QUOTE[num])
+        st.write("by: \n", df.AUTHOR[num])
+        time.sleep(2)
         x+=1
+        st.empty()
+
+    # while True:
+    #     num = randomGenNum()
+    #     st.write("Your quote: \n", df.Quote[num],"\n", "by: \n", df.Author[num])
+    #     time.sleep(2)
+
+
+    st.button("rerun")
+
+
 
 def randomGenNum():
-    line_Num = random.randint(1,1666) # working 
+    line_Num = random.randint(1,75968) # working 
+    # line_Num = random.randint(1,8) # working 
+
     print()
     return line_Num
 
 def main():
     # st.write()
+    st.title("Your Motivation")
+    st.subheader("Here are some quotes to keep you going strong")
+
     openF()
 
 
