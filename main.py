865926092ae283ed4efcea666f64a79acd054931
cheng2 <cheng2@allegheny.edu>
2022-10-17 10:19:01 -0400

    adding code

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import random
 import pandas as pd
+# import streamlit as st
+
 
 def openF():
+    
     df = pd.read_csv('quotes.csv' , delimiter=',')
-    num = randomGenNum()
-    print()
-    print("by: ",df.values[num])
-    print()
 
+    # num = randomGenNum()
+
+    # print(df.Author[num])
+    # print(df.Quote[num])
+
+    # print("Your quote: \n", df.Quote[num], "by: \n", df.Author[num])
+
+    # print using st.write. 
+
+    x = 1
+    y = 8
+    for x in range(1, y):
+        num = randomGenNum()
+        print("Your quote: \n", df.Quote[num],"\n", "by: \n", df.Author[num])
+        x+=1
 
 def randomGenNum():
-    quote_Num = random.randint(1,1666) # working 
+    line_Num = random.randint(1,1666) # working 
     print()
-    return quote_Num
+    return line_Num
 
 def main():
+    # st.write()
     openF()
 
 
